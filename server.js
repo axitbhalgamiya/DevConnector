20ba3a6fe07302e37ecfbd870face03765e507cb
axitbhalgamiya <axitnb9999@gmail.com>
2021-08-03 13:59:24 +0530

    fds

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
-const connectDB = require('./config/db');
-
+const { Int32 } = require('mongodb');
+const mongoose = require('mongoose');
+const connectDB = require('./routes/config/db');
 const app = express();
-
 connectDB();
+
+var StudentSchema = mongoose.Schema({
+    Name: { type : String},
+    no: Int,
+    Department: String
+}, {collection: "Student"});
+
+var employeeData = mongoose.model('Student', empSchema);
+console.log("mydata", employeeData.find({}).then(res => console.log(res)));
+
 app.get('/',(req,res) => res.send('API  Running'));
 
 const PORT = process.env.PORT || 5050;
