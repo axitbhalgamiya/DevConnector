1099ecfe3f5a5f1aef33b7c01251499e38298bfc
axitbhalgamiya <axitnb9999@gmail.com>
2021-08-03 15:10:58 +0530

    com

--- server.js
+++ server.js
@@ -15,10 +15,11 @@ var employeeData = mongoose.model('Student', StudentSchema);
 console.log("mydata", employeeData.find({}).then(res => console.log(res)));
 
 app.get('/',(req,res) => res.send('API  Running'));
-app.use('./api/users',require('./routes/api/users'));
-app.use('./api/auth',require('./routes/api/auth'));
-app.use('./api/posts',require('./routes/api/posts'));
-app.use('./api/profile',require('./routes/api/profile'));
+
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/posts',require('./routes/api/posts'));
+app.use('/api/profile',require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5050;
 
