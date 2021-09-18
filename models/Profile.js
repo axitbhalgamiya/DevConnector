dafb41d6e8c0fbe7d410679c3d1d7c0bec672612
axitbhalgamiya <axitnb9999@gmail.com>
2021-09-18 14:33:33 +0530

    22

--- models/Profile.js
+++ models/Profile.js
@@ -67,7 +67,7 @@ const ProfileSchema = new mongoose.Schema({
                 type:String,
                 required:true
             },
-            fieldstudy:{
+            fieldofstudy:{
                 type:String,
                 required:true
             },
